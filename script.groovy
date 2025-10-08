b9e1b98d09d3efb09780b039dffd3eea774d221a
danimatuko <danimatuko@outlook.com>
2025-10-08 15:27:48 +0300

    fix image name and tag

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'docker build -t danimatuko/demo-app:jma-1.3 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push danimatuko/demo-app:jma-1.3'
     }
 }
 
